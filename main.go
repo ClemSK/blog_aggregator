21f593ad5690645343a98c87fe85536c17fcc981
Clement Seconde-Kynnersely <c.sec.kyn@gmail.com>
2024-01-22 23:15:29 +0000

    feat(server): boilerplate complete

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Print("Hello, blog you\n")
-
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
@@ -21,5 +20,29 @@ func main() {
 
 	router := chi.NewRouter()
 
-	fmt.Println("PORT: ", portString)
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
